c70124887af029f4e07943ae42f133785bfcc599
Asuka Nakagawa <nina-jamie.elle14@live.jp>
2016-11-12 19:35:01 -0800

    added DetailVC

---
+++ AlertFriends/DetailContactViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailContactViewController.swift
+//  AlertFriends
+//
+//  Created by Asuka Nakagawa on 2016-11-12.
+//  Copyright © 2016 Kelvin. All rights reserved.
+//
+
+import UIKit
+
+class DetailContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
