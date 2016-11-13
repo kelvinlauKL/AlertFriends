da5ebcfdfb2f881954ea98eca681eca76caeb825
Kelvin Lau <kelvin.kl.lau@gmail.com>
2016-11-12 16:34:09 -0800

    Added some more events.

--- AlertFriends/Server+Events.swift
+++ AlertFriends/Server+Events.swift
@@ -8,7 +8,11 @@
 
 extension Server {
   static func fetchEvents(completion: @escaping ([Event]) -> ()) {
-    let event1 = Event(name: "Grocery Shopping", image: #imageLiteral(resourceName: "groceryShopping"), threatLevel: .low, duration: 1000)
-    completion([event1])
+    let event1 = Event(name: "Grocery Shopping", image: #imageLiteral(resourceName: "groceryShopping"), threatLevel: .low, duration: 3600)
+    let event2 = Event(name: "Party", image: #imageLiteral(resourceName: "party"), threatLevel: .high, duration: 14400)
+    let event3 = Event(name: "Running", image: #imageLiteral(resourceName: "running"), threatLevel: .high, duration: 3600)
+    let event4 = Event(name: "Shopping", image: #imageLiteral(resourceName: "shopping"), threatLevel: .medium, duration: 14400)
+    let event5 = Event(name: "SightSeeing", image: #imageLiteral(resourceName: "sightseeing"), threatLevel: .medium, duration: 14400)
+    completion([event1, event2, event3, event4, event5])
   }
 }
